a5b542abe042c9242ffeefbd2f7d7f9134093d17
Jennifer Valle <jvallexm@gmail.com>
2020-07-22 11:15:38 -0400

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -5,15 +5,15 @@
 
 resource ibm_container_cluster classic_cluster {
 
-    name              = "${var.cluster_name}"
-    resource_group_id = "${var.resource_group_id}"
-    datacenter        = "${element(var.zones, 0)}"
-    machine_type      = "${var.machine_type}"
-    kube_version      = "${var.kube_version}"
-    hardware          = "${var.hardware}"
-    public_vlan_id    = "${var.private_only ? null : element(var.public_vlan_ids, 0) }"
-    private_vlan_id   = "${element(var.private_vlan_ids, 0)}"
-    default_pool_size = "${var.default_pool_size}"
+    name              = var.cluster_name
+    resource_group_id = var.resource_group_id
+    datacenter        = element(var.zones, 0)
+    machine_type      = var.machine_type
+    kube_version      = var.kube_version
+    hardware          = var.hardware
+    public_vlan_id    = var.private_only ? null : element(var.public_vlan_ids, 0)
+    private_vlan_id   = element(var.private_vlan_ids, 0)
+    default_pool_size = var.default_pool_size
 
 }
 
@@ -25,13 +25,13 @@ resource ibm_container_cluster classic_cluster {
 #############################################################################
 
 resource ibm_container_worker_pool_zone_attachment multi_zone {
-    count             = "${length(var.zones) - 1}"
-    resource_group_id = "${var.resource_group_id}"
-    cluster           = "${ibm_container_cluster.classic_cluster.name}"
-    worker_pool       = "${ibm_container_cluster.classic_cluster.worker_pools.0.id}"
-    zone              = "${element(var.zones, count.index + 1)}"
-    public_vlan_id    = "${var.private_only ? null : element(var.public_vlan_ids, count.index + 1)}"
-    private_vlan_id   = "${element(var.private_vlan_ids, count.index + 1)}"
+    count             = length(var.zones) - 1
+    resource_group_id = var.resource_group_id
+    cluster           = ibm_container_cluster.classic_cluster.name
+    worker_pool       = ibm_container_cluster.classic_cluster.worker_pools.0.id
+    zone              = element(var.zones, count.index + 1)
+    public_vlan_id    = var.private_only ? null : element(var.public_vlan_ids, count.index + 1)
+    private_vlan_id   = element(var.private_vlan_ids, count.index + 1)
 }
 
 #############################################################################
